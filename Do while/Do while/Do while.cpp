c1bd6f587cc99b42aac20239c9001f2e80152f9d
Mitya <dim.maluckow2017@yandex.ru>
2024-05-09 17:33:49 +0300

    first commit

---
+++ Do while/Do while/Do while.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+
+int main()
+{
+
+    setlocale(LC_ALL, "Russian");
+    int a;
+    int b;
+    std::cout << "Введите начало диапазона:\n";
+    std::cin >> a;
+    std::cout << "Введите конец диапазона:\n";
+    std::cin >> b;
+    int sum = 0;
+    do {
+        if (a % 2 != 0) {
+            sum += a;
+        }
+        a++;
+    } while (a <= b);
+    std::cout << "Сумма нечетных чисел из диапазона:\n";
+    std::cout << sum;
+}
+
---
+++ switch/switch/switch.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int c;
+    float a;
+    float b;
+    std::cout << "Введите 2 числа с клавиатуры:\n";
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << "Выберите математическую операцию:\n";
+    std::cout << "1.Сложение\n";
+    std::cout << "2.Вычитание\n";
+    std::cout << "3.Умножение\n";
+    std::cout << "4.Деление\n";
+    std::cin >> c;
+    switch (c) {
+    case 1:
+        std::cout << "Cумма чисел: " << a + b << std::endl;
+        break;
+    case 2:
+        std::cout << "Разность чисел: " << a - b << std::endl;
+        break;
+    case 3:
+        std::cout << "Произведение чисел: " << a * b << std::endl;
+        break;
+    case 4:
+        std::cout << "Частное чисел : " << a / b << std::endl;
+        break;
+    default:
+        std::cout << "Такой операции нет" << std::endl;
+        break;
+    }
+
+}
