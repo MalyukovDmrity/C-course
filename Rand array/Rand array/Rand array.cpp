049a3f26539fc3c959c7b0244b490cc9d49fd435
Mitya <dim.maluckow2017@yandex.ru>
2024-05-26 11:26:06 +0300

    done some homework

---
+++ Rand array/Rand array/Rand array.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <ctime>
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int const SIZE = 10;
+    int arr[SIZE];
+    int arr_min = 100;
+    srand(time(NULL));
+    for (int i = 0; i < SIZE; i++) {
+        arr[i] = rand() % 20;
+        bool exists = false;
+        for (int j = 0; j < i; ++j) {
+            if (arr[j] == arr[i]) {
+                exists = true;
+                break;
+            }
+        }
+
+        if (!exists) {
+            arr[i] = arr[i];
+        }
+        else {
+            --i; 
+        }
+
+    }
+    for (int i = 0; i < SIZE; i++) {
+        std::cout << arr[i] << std::endl;
+    }
+    for (int i = 0; i < SIZE; i++) {
+        if (arr[i] < arr_min) {
+            arr_min = arr[i];
+        }
+    }
+    std::cout << "Минимальный элемент массива равен: " << arr_min;
+    
+}
+
---
+++ nested for loop/nested for loop/nested for loop.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int width;
+    int height;
+    std::cout << "Введите ширину прямоугольника: ";
+    std::cin >> width;
+    std::cout << "Введите высоту прямоугольника: ";
+    std::cin >> height;
+    for (int i = 1; i <= height; i++) {
+        for (int j = 1; j <= width; j++) {
+            std::cout << "*";
+            
+        }
+        std::cout << "\n";
+    }
+}
+
+
